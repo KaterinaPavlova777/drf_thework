5b16daa5065f5c8165d98644fbbf0d2c741df965
Ekaterina Pavlova <miss.kat.katerina2007@gmail.com>
2025-03-18 18:38:50 +0300

    30.2

--- materials/serializers.py
+++ materials/serializers.py
@@ -3,13 +3,27 @@ from rest_framework import serializers
 from materials.models import Course, Lesson
 
 
+class LessonSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Lesson
+        fields = '__all__'
+
+
 class CourseSerializer(serializers.ModelSerializer):
+    lessons = LessonSerializer(many=True)
+
     class Meta:
         model = Course
         fields = '__all__'
 
 
-class LessonSerializer(serializers.ModelSerializer):
+class CourseDetailSerializer(serializers.ModelSerializer):
+    quantity_lessons = serializers.SerializerMethodField()
+    lessons = LessonSerializer(many=True)
+
+    def get_quantity_lessons(self, course):
+        return Lesson.objects.filter(course=course).count()
+
     class Meta:
-        model = Lesson
-        fields = '__all__'
+        model = Course
+        fields = ('title', 'description', 'quantity_lessons')
--- users/views.py
+++ users/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import viewsets, generics, filters
+
+from users.models import Payments
+from users.serializers import PaymentsSerializer, UserSerializer
+
 
 # Create your views here.
+
+
+class UserCreateAPIView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+
+
+class PaymentsViewSet(viewsets.ModelViewSet):
+    serializer_class = PaymentsSerializer
+    queryset = Payments.objects.all()
+    filter_backends = [filters.OrderingFilter]
+    filterset_fields = ['course', 'lesson', 'payment_method']
+    ordering_fields = ['payment_date']
