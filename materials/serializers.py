532da2bf9ee7d0fb7037617f167b92768efcd4b7
Ekaterina Pavlova <miss.kat.katerina2007@gmail.com>
2025-04-02 17:19:32 +0300

    32.1

--- materials/serializers.py
+++ materials/serializers.py
@@ -1,12 +1,14 @@
 from rest_framework import serializers
 
-from materials.models import Course, Lesson
+from materials.models import Course, Lesson, Subscription
+from materials.validators import LinkValidator
 
 
 class LessonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lesson
         fields = '__all__'
+        validators = [LinkValidator(field='link')]
 
 
 class CourseSerializer(serializers.ModelSerializer):
@@ -20,10 +22,18 @@ class CourseSerializer(serializers.ModelSerializer):
 class CourseDetailSerializer(serializers.ModelSerializer):
     quantity_lessons = serializers.SerializerMethodField()
     lessons = LessonSerializer(many=True)
+    subscription = serializers.SerializerMethodField()
 
-    def get_quantity_lessons(self, course):
-        return Lesson.objects.filter(course=course).count()
+    def get_subscription(self, course):
+        user = self.context['request'].user
+        return Subscription.objects.all().filter(user=user).filter(course=course).exists()
 
     class Meta:
         model = Course
-        fields = ('title', 'description', 'quantity_lessons')
+        fields = ('title', 'description', 'quantity_lessons', 'subscription')
+
+
+class SubscriptionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Subscription
+        fields = ("sign_of_subscription",)
